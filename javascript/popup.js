711db35fb728a0b38b9b0333288c30fd6705028c
Lais Varejão <laisvarejao@gmail.com>
2015-09-01 14:56:00 -0400

    Renders correct popup actions and style.

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,56 +1,83 @@
+
+window.onload = function() {
+  document.getElementById("save-btn").addEventListener("click", saveCurrentPage);
+}
+
+function saveCurrentPage() {
+  getCurrentTabUrl(function(url) {
+    getPageContent(url, savePage, renderStatus);
+  });
+}
+
 function getCurrentTabUrl(callback) {
   var queryInfo = {
     active: true,
     currentWindow: true
   };
-
   chrome.tabs.query(queryInfo, function(tabs) {
     var tab = tabs[0];
     var url = tab.url;
     console.assert(typeof url == 'string', 'tab.url should be a string');
-    
-    chrome.storage.local.get({urls: []}, function(result) {
-      var urls = result.urls;
-      urls.push({url: url, done: false});
-      chrome.storage.local.set({urls: urls});
-      callback(url);
-    });
+    // console.assert(/^https?:\/\//i.test(url), 'not a valid url');
+    callback(url);
   });
 }
 
-// function getPageContent(url, callback, errorCallback) {
-//   var request = new XMLHttpRequest();
-//   request.open("GET", url, true);
-//   request.send(null); //why is this needed?
+function getPageContent(url, callback, errorCallback) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.send(null); //why is this needed?
 
-//   request.onload = function() {
-//     var response = request.responseText;
-//     if (!response) {
-//       errorCallback('No response from the page!');
-//       return;
-//     }
+  request.onload = function() {
+    var response = request.responseText;
+    if (!response) {
+      errorCallback('No response from the page!');
+      return;
+    }
 
-//     var parser = new DOMParser();
-//     var doc = parser.parseFromString(response, "text/html");
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(response, "text/html");
 
-//     //TODO
-//     var object = null;
-//     callback(object);
-//   };
+    var titleNode = doc.getElementsByTagName("title")[0];
+    var title = titleNode.childNodes[0].textContent;
 
-//   request.onerror = function() {
-//     errorCallback('Network error.');
-//   };
-// }
+    try {
+      var imageNode = doc.getElementsByTagName("img")[0];
+      var image = imageNode.src;
+    } catch(err){}
+
+    callback(url, title, image);
+  };
+
+  request.onerror = function() {
+    errorCallback('Network error.');
+  };
+}
 
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    renderStatus('Saving... ' + url);
-  });
-});
+function savedAlready(urls, url) {
+  urls.forEach(function(elem) {
+    if (elem.url == url) {
+      return true;
+    }
+  });  
+  return false;
+}
 
-// What's the limit of callbacks? when to know where to stop?
+function savePage(url, title, image) {
+  chrome.storage.local.get({urls: []}, function(result) {
+    var urls = result.urls;
+    var saved = savedAlready(urls, url);
+    console.log(saved);
+    if (!saved) {
+      urls.push({url: url, title: title, image: image, done: false});
+      chrome.storage.local.set({urls: urls});
+      renderStatus('Saving page ' + url);
+    } else {
+      renderStatus('You already saved this');
+    }
+  });
+}
